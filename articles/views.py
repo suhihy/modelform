a32bcf7b9d69279604ba33106f8c637589fb03d9
changehee <oche.edu@gmail.com>
2024-07-31 14:45:27 +0900

    Update 기능구현

--- articles/views.py
+++ articles/views.py
@@ -59,3 +59,22 @@ def delete(request, id):
 
     return redirect('articles:index')
     
+
+def update(request, id):
+    article = Article.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        # article = Article.objects.get(id=id)
+        form = ArticleForm(instance=article)
+        
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'update.html', context)
