9ce0c6e055a42b1fd84fb47ba56d435291fe1cf5
changehee <oche.edu@gmail.com>
2024-07-31 13:50:14 +0900

    Create(Ver.2)기능구현

--- articles/views.py
+++ articles/views.py
@@ -14,35 +14,41 @@ def index(request):
 
 
 def create(request):
-    # new/ => 빈 종이를 보여주는 기능
-    # create/ => 사용자가 입력한 데이터를 저장
+    # 모든 경우의수
+    # - GET : form을 만들어서 html 문서를 사용자에게 리턴
+    #   => 1~4번
+    # - POST invalid data (데이터 검증에 실패한 경우)
+    #   => 5~9번
+    # - POST valid data (데이터 검증에 성공한 경우)
+    #   => 10~14번
 
-    # ========
-
-    # GET create/ => 빈 종이를 보여주는 기능
-    # POST create/ => 사용자가 입력한 데이터를 저장
-
+    # 5. POST 요청 (데이터가 잘 못 들어온 경우)
+    # 10. POST 요청 (데이터가 잘 들어온 경우)
     if request.method == 'POST':
+        # 6. 사용자가 입력한데이터X(request.POST)를 담아서 form을 생성
+        # 11. 사용자가 입력한데이터O(request.POST)를 담아서 form을 새성
         form = ArticleForm(request.POST)
         
+        # 7. form을 검증(실패)
+        # 12. form을 검증(성공)
         if form.is_valid():
+            # 13. form을 저장
             form.save()
+            # 14. index페이지로 redirect
             return redirect('articles:index')
-        else:
-            # form = ArticleForm(request.POST)
-            context = {
-                'form': form,
-            }
-
-            return render(request, 'create.html', context)
-
+    # 1. GET 요청
     else:
+        # 2. 비어있는 form을 만들어서
         form = ArticleForm()
 
-        context = {
-            'form': form,
-        }
+    # 3. context dict에 비어있는 form을 담아서
+    # 8. context dict에 검증에 실패한 form을 담아서
+    context = {
+        'form': form,
+    }
 
-        return render(request, 'create.html', context)
+    # 4. create.html을 랜더링
+    # 9. create.html을 랜더링
+    return render(request, 'create.html', context)
 
 
